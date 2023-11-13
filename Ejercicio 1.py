b8e9ad3fc12198e10f0200130202b06de573e3d4
bblandoesp <bblandoesp@educacion.navarra.es>
2023-11-13 19:54:07 +0100

    Bienvenida

---
+++ Ejercicio 1.py
@@ -0,0 +1,11 @@
+nombre = input ("Nombre:")
+def nombre_del_usuario (nombre):
+    """Funcion que pide el nombre de un usuario
+    
+    parametro:
+    -nombre: Nombre de un usuario cualquiera pedida anteriormente
+    salida:
+        Un mensaje de bienvenida + el nombre del usuario"""
+    print ("¡Hola", nombre + "!")
+    return
+nombre_del_usuario (nombre)
